6a6f30ae4e45eeceaced17af7d80b2d3f10832c4
Cake_C <c390656112@163.com>
2020-07-06 16:07:00 +0800

    更新01

--- MyScrapyProject/MyScrapyProject/spiders/Spider01.py
+++ MyScrapyProject/MyScrapyProject/spiders/Spider01.py
@@ -5,9 +5,12 @@ class Myscrapy01Spider(scrapy.Spider):
     name = 'MyScrapy01'
     allowed_domains = [
         'quotes.toscrape.com',
-        'pixiv.net/'
+        'pixiv.net'
     ]
-    start_urls = ['www.pixiv.net']
+    start_urls = [
+        'www.pixiv.net'
+        'https://www.pixiv.net/users/45273568/bookmarks/artworks'
+    ]
 
     def parse(self, response):
         self.logger.debug(response.text)
---
+++ 下载依赖(复制到Scripts中使用).bat
@@ -0,0 +1,4 @@
+pip install flask -i https://douban.com/simple
+pip install flask_moment -i https://mirrors.aliyun.com/pypi/simple/
+pip install scrapy -i https://mirrors.aliyun.com/pypi/simple/
+pip install fake_useragent -i https://douban.com/simple
