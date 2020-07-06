5a32c474de6da222418bc2740082b77aa6273631
Cake_C <c390656112@163.com>
2020-07-06 17:44:59 +0800

    现在爬虫中 能爬到用户的收藏信息了

--- MyScrapyProject/MyScrapyProject/spiders/Spider01.py
+++ MyScrapyProject/MyScrapyProject/spiders/Spider01.py
@@ -1,6 +1,7 @@
 import scrapy
 import requests
 import re
+from bs4 import BeautifulSoup
 class Myscrapy01Spider(scrapy.Spider):
     name = 'MyScrapy01'
     allowed_domains = [
@@ -8,16 +9,15 @@ class Myscrapy01Spider(scrapy.Spider):
         'pixiv.net'
     ]
     start_urls = [
-        'https://www.pixiv.net/'
-        #'https://www.pixiv.net/users/45273568/bookmarks/artworks'
+        'https://www.pixiv.net/',
+        'https://www.pixiv.net/users/45273568/bookmarks/artworks'
     ]
 
     def parse(self, response):
         with requests.Session() as self.s:
             print("******  start  ******")
             self.login()
-            self.main()
-        print('now Downloading the page')
+            self.mainProcess()
         
         
     def login(self):
@@ -49,6 +49,107 @@ class Myscrapy01Spider(scrapy.Spider):
         else:
             raise Exception("post key can not found")
     
+    def get_account():
+    #"""
+    #config example:{"password": "123", "pixiv_id": "123@qq.com"}
+    #:return:
+    #"""
+        try:
+            file = open('config', 'r')
+            str_data = file.read()
+            file.close()
+            data = json.loads(str_data)
+            return data['pixiv_id'], data['password']
+        except IOError:
+            raise Exception("user config error")
+    
+    def mainProcess(self):
+        self.get_user_bookmark(45273568)
+        pass
+    
+    
+    def get_user_bookmark(self,user_id):
+    #"""
+    #获取用户收藏的作品，并根据收藏作品的创作者id加入user_id(需要判断是否已经在user_ids里)末尾
+    #:param user_id:
+    #:return:
+    #"""
+        print("****** 开始获取用户（%d）的收藏作品 ******" % user_id)
+
+        i = 1
+        count = 0
+        while True:
+            try:
+                print('尝试下载用户页面')
+                url_tmp_s="https://www.pixiv.net/users/"+str(user_id)+"/bookmarks/artworks"
+                print(url_tmp_s)
+                res = self.s.get(url_tmp_s)
+                print(res.text)
+                print('尝试使用BF4')
+                soup = BeautifulSoup(res.text, "html.parser")
+                all_img = soup.find_all('img', class_="ui-scroll-view")
+            except self.e:
+                print(self.e)
+            
+        # 如果未找到，则说明已到最大页数
+            if not all_img:
+                break
+
+        # 查找收藏图片的作者id
+            all_a = soup.find_all('a', class_="ui-profile-popup")
+            for a in all_a:
+                c_user_id = a['data-user_id']
+                add_glob_user_ids(c_user_id)
+
+            print("用户（%d）收藏，已存：%d，新增：%d" % (user_id, count, len(all_img)))
+            for img in all_img:
+                target_url = img['data-src']
+                try:
+                    if not download("bookmarks", target_url):
+                        console("ERROR:" + " bookmarks " + target_url)
+                        save_error(user_id, target_url)
+                except IndexError as e:
+                    print (e)
+                    print(target_url)
+
+            count += len(all_img)
+            i += 1
+    
+    
+    
+    
+    
+    
+    
     def main(self):
+        global user_ids, index
+        user_ids, index, work_or_mark = _get_ckpt()
+
+        if not user_ids:
+            console("start get user ids")
+            user_ids = _get_user_ids()
+        else:
+            console("read from ckpt. users count: %d, index: %d" % (len(user_ids), index))
+
+        console("开始遍历 user。 start index : " + str(index))
+
+        first = True
+        while index < len(user_ids):
+            if not (first and work_or_mark):
+                console("从用户的作品开始")
+                _save_ckpt(0)
+                # 抓取该用户的作品
+                get_user_work(user_ids[index])
+            else:
+                console("从用户收藏的作品开始")
+
+            _save_ckpt(1)
+            # 抓取该用户收藏的作品
+            get_user_bookmark(user_ids[index])
+
+            first = False
+            index += 1
+
+        console("全部抓取完成，用户总数：" + str(len(user_ids)))
         
         
--- 下载依赖(复制到Scripts中使用).bat
+++ 下载依赖(复制到Scripts中使用).bat
@@ -3,3 +3,4 @@ pip install flask_moment -i https://mirrors.aliyun.com/pypi/simple/
 pip install scrapy -i https://mirrors.aliyun.com/pypi/simple/
 pip install fake_useragent -i https://douban.com/simple
 pip install requests -i https://mirrors.aliyun.com/pypi/simple/
+pip install beautifulsoup4 -i https://mirrors.aliyun.com/pypi/simple/
