b2e07f4865c086741f7edf36edf9afbed1fc807b
西行寺静 <65853669+SaigyoujiShizuka@users.noreply.github.com>
2020-07-08 12:31:50 +0800

    添加服务器记录的日志函数

--- MyScrapyProject/MyScrapyProject/MysqlRW.py
+++ MyScrapyProject/MyScrapyProject/MysqlRW.py
@@ -2,11 +2,28 @@ from MyScrapyProject.items import UserAccount
 from itemadapter import ItemAdapter
 import pymysql.cursors
 
+Logtype=("登录","注册","修改个人信息","绑定","添加收藏","移除收藏","添加关注","移除关注")
+
+
+
 class SQLOS():
     def __init__(self):
         pass
 
-
+    def WritetoLog(Userid,type,LogCentent):
+        db=SQLOS.Connect_to_DB()
+        cursor=db.cursor()
+    
+        sql_write="INSERT INTO d_log (`userid`,`logtype`,`logcontent`,`logtime`) VALUES(%s,%s,%s,%s)"
+        try:
+           
+            cursor.execute(sql_write,(Userid,Logtype[type],LogCentent,datetime.datetime.today()))
+          
+            db.commit()
+        except:
+            
+            db.rollback()
+        db.close()#生成日志用函数，传入三个参数，用户ID、日志类别、日志信息
 
     def Connect_to_DB():
         connection = pymysql.connect(host='rm-bp10wr08s7nl319dcyo.mysql.rds.aliyuncs.com',
@@ -23,18 +40,26 @@ class SQLOS():
         sql_write = "INSERT INTO d_user_account (`PixivID`,`Pixivpw`,`Username`,`Userpw`,`Usermode`,`create_date`,`lastlogindate`,`lastloginip`,`logincount`) VALUES (%s,%s,%s,%s,%s,%s,%s,%s,%s)"
        
         try:
-    
+         
             cursor.execute(sql_write,(UserAccount.get("PixivID",""),UserAccount.get("Pixivpw",""),UserAccount.get("Username",""),UserAccount.get("Userpw",""),UserAccount.get("Usermode",""),UserAccount.get("Create_date",""),UserAccount.get("Lastlogindate",""),UserAccount.get("Lastloginip",""),UserAccount.get("Logincount","")))
-        
-      
+            cursor.execute("SELECT LAST_INSERT_ID() from d_user_account as ID")
+            flag=cursor.fetchall()
+           
+            print(db)
             db.commit()
+            db.close()
+            SQLOS.WritetoLog(flag[0]['LAST_INSERT_ID()'],1,"注册成功!")
+            
+      
+           
             return 1
         except:
-        
+            print(444)
             db.rollback()
+            db.close()
             return 0
-         
-        db.close()#参数为UserAccount 向表中添加一个对象，如果成功返回1，失败返回0
+            
+        #参数为UserAccount 向表中添加一个对象，如果成功返回1，并向日志中写一条注册成功日志。失败返回0
     def EditUserAccount(ID,UserAccount):
         db=SQLOS.Connect_to_DB()
         cursor=db.cursor()
@@ -50,7 +75,7 @@ class SQLOS():
                 return 0
         else:
             return -1#更改制定ID的用户账户信息，成功返回1，失败返回0，未找到ID返回-1
-    def GetUserAccount(ID):#得到制定ID的用户账户信息，成功返回账户信息，未找到ID返回-1
+    def GetUserAccount(ID):
         db=SQLOS.Connect_to_DB()
         cursor=db.cursor()
         UserAC=UserAccount()
@@ -69,4 +94,13 @@ class SQLOS():
             return UserAC
         else:
             return -1
-        db.close()
+        db.close()#得到制定ID的用户账户信息，成功返回账户信息，未找到ID返回-1
+    def SwitchUserMode(ID):
+        User=SQLOS.GetUserAccount(ID)
+        User['Usermode']=not User['Usermode']
+        SQLOS.EditUserAccount(ID,User) #更改用户模式
+        SQLOS.WritetoLog(ID,2,("修改账户类型为 %s"% (not User['Usermode'])))#更改账户类型，并向日志中写入一条记录
+    
+
+
+
